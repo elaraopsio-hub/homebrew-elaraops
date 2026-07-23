bac042d457293d40b03baf9b0d6951f7af6ae5ee
Elaraops <elaraops.io@gmail.com>
2026-07-23 08:48:15 -0500

    Update elaraops.rb

--- Casks/elaraops.rb
+++ Casks/elaraops.rb
@@ -1,14 +1,14 @@
- cask "elaraops" do
+cask "elaraops" do
     version "1.0.0"
 
     on_arm do
       url "https://github.com/elaraopsio-hub/elaraops-codebase/releases/download/v#{version}/ElaraOps-#{version}-arm64.dmg"
-      sha256 "d3b0bc77ad274f0c0e48a4878f9bd083a2996b82b5649e5978b55db97a9d56f2"
+      sha256 "3c9e5e835b28651b3308b5f4d71bf1810fc91febf21eb6a806869f30cf019338"
     end
 
     on_intel do
       url "https://github.com/elaraopsio-hub/elaraops-codebase/releases/download/v#{version}/ElaraOps-#{version}.dmg"
-      sha256 "d3b0bc77ad274f0c0e48a4878f9bd083a2996b82b5649e5978b55db97a9d56f2"
+      sha256 "9c626b95d7dd1b7e4072094471973a8032d362e72d934b89c11be5ca8e3bf20a"
     end
 
     name "ElaraOps"
